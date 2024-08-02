1d6f024312ba44f3720a1270369720bad84ba5cb
JAESOON KIM <winnercup1602@gmail.com>
2024-08-02 18:08:47 +0900

    feat: spring boot basic scaffloding

---
+++ src/main/java/com/example/comitserver/domain/Member.java
@@ -0,0 +1,23 @@
+package com.example.comitserver.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private String password;
+}
