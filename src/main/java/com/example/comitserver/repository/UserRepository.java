a6f8a402b7b0d3d63b8e55af4a73196810b8fadf
Kwon Seojin <seojin3154@naver.com>
2024-08-11 16:12:13 +0900

    feat: implement basic user and join

---
+++ src/main/java/com/example/comitserver/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.comitserver.repository;
+
+import com.example.comitserver.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+}
