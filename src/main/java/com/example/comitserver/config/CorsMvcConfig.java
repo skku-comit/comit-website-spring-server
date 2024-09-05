6bcef7ae1b8800da87dca68e68b76e918c774f8a
Kwon Seojin <seojin3154@naver.com>
2024-09-05 16:15:19 +0900

    hotfix: edit cors config

--- src/main/java/com/example/comitserver/config/CorsMvcConfig.java
+++ src/main/java/com/example/comitserver/config/CorsMvcConfig.java
@@ -9,13 +9,14 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .exposedHeaders("Set-Cookie")
+                .exposedHeaders("Set-Cookie") // 필요한 헤더를 명시적으로 노출
                 .allowedOrigins(
                         "http://localhost:3000",
                         "https://skku-comit.dev",
                         "https://comit-website.vercel.app/"
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
-
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true); // 쿠키 및 자격 증명 허용
     }
 }
