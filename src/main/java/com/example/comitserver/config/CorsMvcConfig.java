145b809c480b7947e1f1bf2aec4f99976761cb47
Kwon Seojin <seojin3154@naver.com>
2024-09-05 16:36:14 +0900

    hotfix: edit cors config

--- src/main/java/com/example/comitserver/config/CorsMvcConfig.java
+++ src/main/java/com/example/comitserver/config/CorsMvcConfig.java
@@ -1,22 +1,20 @@
 package com.example.comitserver.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .exposedHeaders("Set-Cookie") // 필요한 헤더를 명시적으로 노출
-                .allowedOrigins(
-                        "http://localhost:3000",
-                        "https://skku-comit.dev",
-                        "https://comit-website.vercel.app/"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true); // 쿠키 및 자격 증명 허용
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
