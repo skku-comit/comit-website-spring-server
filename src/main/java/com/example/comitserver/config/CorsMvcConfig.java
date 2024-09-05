b02db6eb710356aeeffdd44d67889e3500a2db3e
Kwon Seojin <seojin3154@naver.com>
2024-09-05 16:52:23 +0900

    chore: delete annotation in cors config

--- src/main/java/com/example/comitserver/config/CorsMvcConfig.java
+++ src/main/java/com/example/comitserver/config/CorsMvcConfig.java
@@ -1,12 +1,10 @@
 package com.example.comitserver.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
